28fb83c45e9f136e6aff9c91ee3c6cd8090a62fa
tateoeng <tate.o.engstrand@gmail.com>
2018-03-22 02:00:50 -0400

    Update shim.php

--- 0000/00/00/shim.php
+++ 0000/00/00/shim.php
@@ -18,12 +18,12 @@ $html = Markdown::defaultTransform($text);
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Tate's Journal</title>
-				<script type="text/x-mathjax-config">
-				  MathJax.Hub.Config({ tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]}});</script>
-				<script type="text/javascript" src="MathJax/MathJax.js?config=TeX-AMS_HTML-full"></script>
-				<link rel="stylesheet" type="text/css" href="http://localhost/style.css" /></head>
-
+        <title></title>
+	<script type="text/x-mathjax-config">
+	  MathJax.Hub.Config({ tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]}});</script>
+	<script type="text/javascript" src="MathJax/MathJax.js?config=TeX-AMS_HTML-full"></script>
+	<link rel="stylesheet" type="text/css" href="http://localhost/style.css" /></head>
     <body>
-			<div id="content">
-				<?php echo $html; ?></div></body></html>
+	<div id="content">
+	<?php echo $html; ?></div></body></html>
+						
