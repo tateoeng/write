ff86b8936b026af80ec975b6b4cb37e496cbb574
tateoeng <tate.o.engstrand@gmail.com>
2018-03-28 08:33:33 -0400

    shim.php in archive directory now points to the archived page, not current page

--- 0000/00/00/shim.php
+++ 0000/00/00/shim.php
@@ -9,7 +9,13 @@ use Michelf\Markdown;
 date_default_timezone_set('America/New_York');
 
 // Read file and pass content through the Markdown parser
-$p    = 'http://localhost/journal' . date('/Y/m/d/') . 'index.md' ;
+$pth  = realpath('.') . PHP_EOL;
+$qth  = explode("\\", $pth);
+$y    = $qth[9];
+$m    = $qth[10];
+$d    = trim($qth[11]);
+$y    .= '/' . $m . '/' . $d;
+$p    = 'http://localhost/journal/' . $y . '/index.md' ;
 $text = file_get_contents($p);
 
 $html = Markdown::defaultTransform($text);
