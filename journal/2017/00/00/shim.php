ed93cd966b92b071d2fb91c08e49152eb5fd00ed
Tate Owen Engstrand <tate.o.engstrand@gmail.com>
2018-04-04 11:00:59 -0400

    now generates archive page

--- journal/2017/00/00/shim.php
+++ journal/2017/00/00/shim.php
@@ -11,9 +11,9 @@ date_default_timezone_set('America/New_York');
 // Read file and pass content through the Markdown parser
 $pth  = realpath('.') . PHP_EOL;
 $qth  = explode("\\", $pth);
-$y    = $qth[9];
-$m    = $qth[10];
-$d    = trim($qth[11]);
+$d    = trim(array_pop($qth));
+$m    = array_pop($qth);
+$y    = array_pop($qth);
 $y    .= '/' . $m . '/' . $d;
 $p    = 'http://localhost/journal/' . $y . '/index.md' ;
 $text = file_get_contents($p);
@@ -25,8 +25,10 @@ $html = Markdown::defaultTransform($text);
 <html>
     <head>
         <title></title>
+	<script type="text/x-mathjax-config">
+	  MathJax.Hub.Config({ tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]}});</script>
+	<script type="text/javascript" src="MathJax/MathJax.js?config=TeX-AMS_HTML-full"></script>
 	<link rel="stylesheet" type="text/css" href="http://localhost/style.css" /></head>
     <body>
 	<div id="content">
 	<?php echo $html; ?></div></body></html>
-						
